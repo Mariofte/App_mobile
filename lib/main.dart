93575d4571b648713a05ee019fab12ca712a6a2c
Mariofte <150403921+Mariofte@users.noreply.github.com>
2025-02-09 01:09:47 -0600

    App : creat Pages.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
+import 'package:app_mobile/presentation/widgets/Pages.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
 }
-
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App scouting',
-
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 34, 10, 77)), 
-        useMaterial3: true,
-      ),
-
-      
-
+      return MaterialApp(
+        title:  'App scouting',
+        theme: ThemeData(
+          primaryColor: Color.fromARGB(3, 1, 19, 111)
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('App scouting'),),
+          body: Pages(),
+        ),
+        
     );
   }
 }
